c257fc77fdc271336243b5d4d41cc60358265b23
Ally Dale <vipally@gmail.com>
2017-12-03 15:09:21 +0800

    refer local package by [import "#/x/y/z"] style

---
+++ src/go/build/testdata/vroot/gopath3/src/noroot1/vendor/localrootv1/src/local1/util.go
@@ -0,0 +1,25 @@
+package local1
+
+/* test
+import (
+	"path/filepath"
+	"runtime"
+)
+
+func PackageMain() {
+
+}
+
+func GetPackagePath() string {
+	depth := 0
+	if _, __file, _, __ok := runtime.Caller(depth); __ok {
+		thisFilePath := filepath.Dir(__file)
+		return thisFilePath
+	}
+	return ""
+} //
+//*/
+
+func init() {
+
+}
